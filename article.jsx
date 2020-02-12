df2d49c31237955f828c3f415e720c10be5aa1f1
Marek Magdziak <marek.magdziak@telia.no>
2020-02-12 12:25:37 +0100

    Add interactiveness

--- article.jsx
+++ article.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Heading, Tags, ShadowBox, Button } from '@telia/styleguide';
 
-const Article = ({ title, tags, children }) => (
+const Article = ({ title, tags, onOpen, children }) => (
     <ShadowBox>
         <Heading level={1}>{title}</Heading>
         <Tags color="dark-grey">{tags.map(tag => <Tags.Tag key={tag}>{tag}</Tags.Tag>)}</Tags>
         <div>
             {children}
         </div>
-        <Button text="Read more" />
+        <Button onClick={onOpen} text="Read more" />
     </ShadowBox>
 );
 
--- index.jsx
+++ index.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import '@telia/styleguide/index.css';
 
 import Article from './article';
-import { Container } from '@telia/styleguide';
+import { Container, ModalDialogProvider, ModalDialog } from '@telia/styleguide';
 
-const App = () => (
-    <Container>
-        <Article title="My awesome article" tags={[ 'article', 'about', 'stuff' ]}>
-            Here we can put the content of an article
-            <p style={{ color: 'green' }}>Nested elements are also OK</p>
-        </Article>
-        <Article title="My awesome article" tags={[ 'article', 'about', 'stuff' ]}>
-            Here we can put the content of an article
-            <p style={{ color: 'green' }}>Nested elements are also OK</p>
-        </Article>
-    </Container>
+const ArticleDialog = ({ onSubmit }) => (
+    <ModalDialog onSubmit={onSubmit} heading="Article" submitText="Well, OK">
+        Sorry, nothing's gonna be shown here
+    </ModalDialog>
 );
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const App = () => {
+    const [displayArticle, setDisplayArticle] = useState(false);
+
+    return (
+        <Container>
+            <Article onOpen={() => setDisplayArticle(true)} title="My awesome article" tags={[ 'article', 'about', 'stuff' ]}>
+                Here we can put the content of an article
+                <p style={{ color: 'green' }}>Nested elements are also OK</p>
+            </Article>
+            <Article onOpen={() => setDisplayArticle(true)} title="My awesome article" tags={[ 'article', 'about', 'stuff' ]}>
+                Here we can put the content of an article
+                <p style={{ color: 'green' }}>Nested elements are also OK</p>
+            </Article>
+            {displayArticle && <ArticleDialog onSubmit={() => setDisplayArticle(false)} />}
+        </Container>
+    );
+}
+
+ReactDOM.render(
+    <ModalDialogProvider>
+        <App />
+    </ModalDialogProvider>,
+    document.getElementById('app'));
